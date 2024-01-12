1795c032e84b777ebf8ca9c8ca4a3e3e44962b74
Ronila Sanjula <84113874+Roni-Boiz@users.noreply.github.com>
2024-01-12 22:34:16 +0530

    Update hbd.js

--- hbd.js
+++ hbd.js
@@ -283,6 +283,26 @@ function drawName(name, letterColors) {
     pointCollection.points = g;
     initEventListeners();
 }
+
+function wrapText(context, text, x, y, maxWidth, lineHeight){
+    
+    var words = text.split(' ');
+    var line = '';
+    for(var n = 0; n < words.length; n++) {
+      var testLine = line + words[n] + ' ';
+      var metrics = context.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > maxWidth && n > 0) {
+        context.fillText(line, x, y);
+        line = words[n] + ' ';
+        y += lineHeight;
+      }
+      else {
+        line = testLine;
+      }
+    }
+    context.fillText(line, x, y);
+}
  
 window.reset = false;
  
@@ -335,4 +355,5 @@ else
 }
 
 bounceBubbles();
+wrapText(ctx, birthday, x, y, 1000, 500)
 
